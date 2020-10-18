7a1d20b2dde2b2cbedf8c46e0ce7c483e59daa2c
jackiechen <chenjie_nj@126.com>
2020-10-18 23:24:36 +0800

    MergeKList

---
+++ src/main/java/indi/jackie/leetcode/linkedlist/MergeKList23.java
@@ -0,0 +1,32 @@
+package indi.jackie.leetcode.linkedlist;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * @author jackie chen
+ * @create 2020/10/17
+ * @description MergeKList23
+ */
+public class MergeKList23 {
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        ListNode head = new ListNode();
+        ListNode tail = head;
+        PriorityQueue<ListNode> queue = new PriorityQueue<>(Comparator.comparingInt(o -> o.val));
+        List<ListNode> listNodes = Arrays.stream(lists).filter(Objects::nonNull).collect(Collectors.toList());
+        if (listNodes.isEmpty()){
+            return null;
+        }
+        queue.addAll(listNodes);
+        while (!queue.isEmpty()) {
+            ListNode node = queue.poll();
+            tail.next = node;
+            tail = node;
+            if (Objects.nonNull(node.next)) {
+                queue.add(node.next);
+            }
+        }
+        return head.next;
+    }
+}
---
+++ src/test/java/indi/jackie/leetcode/linkedlist/MergeKList23Test.java
@@ -0,0 +1,47 @@
+package indi.jackie.leetcode.linkedlist;
+
+import org.junit.Test;
+
+import java.util.Objects;
+
+/**
+ * @author jackie chen
+ * @create 2020/10/18
+ * @description MergeKList23Test
+ */
+public class MergeKList23Test {
+
+    @Test
+    public void mergeKLists() {
+        ListNode[] lists = createNodes();
+        ListNode node = new MergeKList23().mergeKLists(lists);
+        while (Objects.nonNull(node)){
+            System.out.print(node.val);
+            if (Objects.nonNull(node.next)){
+                System.out.print("-->");
+            }
+            node = node.next;
+        }
+    }
+
+    private ListNode[] createNodes() {
+        ListNode node1 = createNode(new int[]{1, 4, 5});
+        ListNode node2 = createNode(new int[]{1, 3, 4});
+        ListNode node3 = createNode(new int[]{2, 6});
+//        return new ListNode[]{node1, node2, node3};
+        ListNode node4 = createNode(new int[]{});
+        return new ListNode[]{node4};
+    }
+
+    private ListNode createNode(int[] ints) {
+        ListNode head = new ListNode();
+        ListNode tail = head;
+        for (int anInt : ints) {
+            ListNode node = new ListNode();
+            node.val = anInt;
+            tail.next = node;
+            tail = node;
+        }
+        return head.next;
+    }
+}
