c617ea9c03793cf2bd097a8597540e23365947f9
jackiechen <chenjie_nj@126.com>
2020-11-23 10:59:09 +0800

    triangle 120 超时修复 改用字底向上的dp

--- src/main/java/indi/jackie/leetcode/dp/Triangle120.java
+++ src/main/java/indi/jackie/leetcode/dp/Triangle120.java
@@ -8,28 +8,41 @@ import java.util.List;
  * @description Triangle120
  */
 public class Triangle120 {
+    // 自顶向下会超时
+//    public int minimumTotal(List<List<Integer>> triangle) {
+//        //f[i,j] = min(f[i-1,j-1], f[i-1,j]) + triangle[i,j]
+//        int size = triangle.size();
+//        int result = Integer.MAX_VALUE;
+//        for (int i = 0; i < size; i++) {
+//            int value = calculate(triangle, size - 1, i);
+//            if (value < result) {
+//                result = value;
+//            }
+//        }
+//        return result;
+//    }
+//
+//    private int calculate(List<List<Integer>> triangle, int row, int col) {
+//        if (col > row) {
+//            return Integer.MAX_VALUE;
+//        }
+//        if (col < 0) {
+//            return row < 0 ? 0 : Integer.MAX_VALUE;
+//        }
+//        int temp1 = calculate(triangle, row - 1, col - 1);
+//        int temp2 = calculate(triangle, row - 1, col);
+//        return triangle.get(row).get(col) + Math.min(temp1, temp2);
+//    }
+
     public int minimumTotal(List<List<Integer>> triangle) {
         //f[i,j] = min(f[i-1,j-1], f[i-1,j]) + triangle[i,j]
         int size = triangle.size();
-        int result = Integer.MAX_VALUE;
-        for (int i = 0; i < size; i++) {
-            int value = calculate(triangle, size - 1, i);
-            if (value < result) {
-                result = value;
+        int[] mini = new int[triangle.get(size - 1).size() + 1];
+        for (int i = size - 1; i >= 0; i--) {
+            for (int j = 0; j < triangle.get(i).size(); j++) {
+                mini[j] = triangle.get(i).get(j) + Math.min(mini[j], mini[j + 1]);
             }
         }
-        return result;
-    }
-
-    private int calculate(List<List<Integer>> triangle, int row, int col) {
-        if (col > row) {
-            return Integer.MAX_VALUE;
-        }
-        if (col < 0) {
-            return row < 0 ? 0 : Integer.MAX_VALUE;
-        }
-        int temp1 = calculate(triangle, row - 1, col - 1);
-        int temp2 = calculate(triangle, row - 1, col);
-        return triangle.get(row).get(col) + Math.min(temp1, temp2);
+        return mini[0];
     }
 }
