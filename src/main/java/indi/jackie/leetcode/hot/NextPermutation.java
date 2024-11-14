f4fcfba7526b77b69b39bfdb6d96f3ecb34205b1
chenj853 <chenjie_nj@126.com>
2024-11-14 09:24:49 +0800

    hot NextPermutation

---
+++ src/main/java/indi/jackie/leetcode/hot/NextPermutation.java
@@ -0,0 +1,40 @@
+package indi.jackie.leetcode.hot;
+
+/**
+ * 下一个排列
+ *
+ * @author jackie chen
+ * @date 2024/11/12 5:14 PM
+ **/
+public class NextPermutation {
+
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (j >= 0 && nums[i] >= nums[j]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1);
+    }
+
+    private void reverse(int[] nums, int start) {
+        int left = start, right = nums.length - 1;
+        while (left < right) {
+            swap(nums, left, right);
+            left++;
+            right--;
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
